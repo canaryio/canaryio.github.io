68c3c072efce604d0d863c491d497d5226a32e7a
Brandon Tindle <brandon.tindle@gmail.com>
2014-06-07 13:33:05 -0500

    Removed references to EC2

--- public/js/site.js
+++ public/js/site.js
@@ -6,19 +6,7 @@ function closeSignup () {
     $("#signup").css("display", "none");
 }
 
-function showThankyou () {
-  $("#thankyou").css("display", "inline");
-}
 
-function closeThankyou () {
-  $("#thankyou").css("display", "none");
-}
 
-function sendMail() {
-    var myName = $('#uName').val();
-    var myEmail = $('#uEmail').val();
-  $.get("http://54.200.206.48/pass?user=" + myName + "&email=" + myEmail, function(result) {
-    closeSignup();
-    showThankyou();
-  });
-}
+
+
